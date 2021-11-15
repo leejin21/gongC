ced3acc0626bc1f15858635b3c8448293a55568b
JIN <leejin21c@sookmyung.ac.kr>
2021-11-15 13:22:58 +0900

    [server]: sequelize 연결 성공

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,107 @@
 "use strict";
-// src/app.ts
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// src/app.ts
+var dotenv = __importStar(require("dotenv"));
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var models_1 = require("./models");
+dotenv.config();
+// * APP VARIABLES
+var PORT = parseInt(process.env.PORT, 10) || 5000;
+var HOST = process.env.HOST || "localhost";
 var app = (0, express_1.default)();
+// * APP CONFIGURATION: middleware
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(function (req, res, next) {
+    console.log("Request occur! " + req.method + ", " + req.url);
+    next();
+});
+// TODO ROUTER SETTING
 // get
 app.get("/", function (req, res) {
     res.send("hello express");
 });
 // 5000 포트로 서버 실행
-app.listen(5000, function () {
-    console.log("실행중");
-});
+app.listen(PORT, HOST, function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                console.log("server on: listening on " + HOST + ":" + PORT);
+                // sequelize-db connection test
+                return [4 /*yield*/, models_1.sequelize
+                        .authenticate()
+                        .then(function () { return __awaiter(void 0, void 0, void 0, function () {
+                        return __generator(this, function (_a) {
+                            console.log("seq connection success");
+                            return [2 /*return*/];
+                        });
+                    }); })
+                        .catch(function (e) {
+                        console.log("seq ERROR: ", e);
+                    })];
+            case 1:
+                // sequelize-db connection test
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,41 @@
 // src/app.ts
+import * as dotenv from "dotenv";
+import express, { Response, Request, NextFunction } from "express";
+import cors from "cors";
 
-import express from "express";
+import { sequelize } from "./models";
+
+dotenv.config();
+// * APP VARIABLES
+const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
+const HOST: string = process.env.HOST || "localhost";
 const app: express.Application = express();
 
+// * APP CONFIGURATION: middleware
+app.use(cors());
+app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`Request occur! ${req.method}, ${req.url}`);
+    next();
+});
+
+// TODO ROUTER SETTING
+
 // get
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("hello express");
 });
 
 // 5000 포트로 서버 실행
-app.listen(5000, () => {
-    console.log("server on: listening to port 5000...");
+app.listen(PORT, HOST, async () => {
+    console.log(`server on: listening on ${HOST}:${PORT}`);
+    // sequelize-db connection test
+    await sequelize
+        .authenticate()
+        .then(async () => {
+            console.log("seq connection success");
+        })
+        .catch((e) => {
+            console.log("seq ERROR: ", e);
+        });
 });
