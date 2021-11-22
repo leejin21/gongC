89286b02ee922ef0f6b7102ae44a67ec37bcf7ae
JIN <leejin21c@sookmyung.ac.kr>
2021-11-22 21:39:42 +0900

    [server]: auth 부분 service 레이어로 REFACTOR

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -1,82 +1,29 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = __importStar(require("dotenv"));
-const user_model_1 = __importDefault(require("../models/user.model"));
-const bcryptjs_1 = __importDefault(require("bcryptjs"));
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const auth_services_1 = require("../services/auth-services");
 const signUp = async (req, res) => {
-    // TODO make, save, and send token
     // * Validate user input
     if (!req.body.email || !req.body.password || !req.body.nickname) {
         res.status(400).send({ status: 400, message: "Fail SignUp" });
         return;
     }
     const { email, password, nickname } = req.body;
-    // * Validate if email already exists
-    let isEmailExist = false;
-    await user_model_1.default.findOne({ where: { email: email } })
-        .then((data) => {
-        if (data) {
-            isEmailExist = true;
-            res.status(400).send({
-                status: 400,
-                message: "Email already exist",
-            });
-        }
-    })
-        .catch((e) => {
-        console.log(e);
-        res.status(400).send({ status: 400, message: "Server Error" });
-        return;
-    });
-    // * Create User only when the email does not already exists
-    dotenv.config();
-    const TOKEN_KEY = process.env.TOKEN_KEY || "";
-    if (!isEmailExist) {
-        // * Encrypt user password
-        let encryptedPassword = await bcryptjs_1.default.hash(password, 10);
-        const token = jsonwebtoken_1.default.sign({ email }, TOKEN_KEY, {
-            expiresIn: "999999h",
+    const returnData = await (0, auth_services_1.signUpService)(email, password, nickname);
+    if (returnData.status == 200) {
+        // when successed
+        const { status, message, responseData } = returnData;
+        res.status(status).send({
+            status,
+            message,
+            responseData,
         });
-        new user_model_1.default({
-            email: email || "",
-            password: encryptedPassword || "",
-            nickname: nickname || "",
-            android_token: token || "",
-        })
-            .save()
-            .then((data) => {
-            res.status(200).send({
-                status: 200,
-                message: "SignUp Success",
-                token: data.android_token,
-            });
-        })
-            .catch((e) => {
-            console.log(e);
-            res.status(400).send({ status: 400, message: "Server Error" });
+    }
+    else {
+        // when failed
+        const { status, message } = returnData;
+        res.status(status).send({
+            status,
+            message,
         });
     }
 };
@@ -90,47 +37,24 @@ const login = async (req, res) => {
         return;
     }
     const { email, password } = req.body;
-    await user_model_1.default.findOne({
-        where: {
-            email: email,
-        },
-        attributes: ["id", "password", "android_token"],
-    })
-        .then(async (data) => {
-        // * Validate if email already exists
-        if (data) {
-            const isPasswordCorrect = await bcryptjs_1.default.compare(password, data.password);
-            // * Validate if password is correct
-            if (isPasswordCorrect) {
-                res.status(200).send({
-                    status: 200,
-                    message: "Login Success",
-                    token: data.android_token,
-                });
-            }
-            else {
-                res.status(400).send({
-                    status: 400,
-                    message: "Wrong password",
-                });
-            }
-        }
-        else {
-            res.status(400).send({
-                status: 400,
-                message: "Wrong email",
-            });
-        }
-    }, (e) => {
-        throw e;
-    })
-        .catch((e) => {
-        console.log(e);
-        res.status(400).send({
-            status: 400,
-            message: "Server Error",
+    const returnData = await (0, auth_services_1.loginService)(email, password);
+    if (returnData.status == 200) {
+        // when successed
+        const { status, message, responseData } = returnData;
+        res.status(status).send({
+            status,
+            message,
+            responseData,
         });
-    });
+    }
+    else {
+        // when failed
+        const { status, message } = returnData;
+        res.status(status).send({
+            status,
+            message,
+        });
+    }
 };
 exports.default = {
     signUp: signUp,
--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,11 +1,9 @@
 import { Response, Request } from "express";
-import * as dotenv from "dotenv";
-import User from "../models/user.model";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+
+import { loginService, signUpService } from "../services/auth-services";
+import { serviceReturnForm } from "../modules/service-modules";
 
 const signUp = async (req: Request, res: Response) => {
-    // TODO make, save, and send token
     // * Validate user input
     if (!req.body.email || !req.body.password || !req.body.nickname) {
         res.status(400).send({ status: 400, message: "Fail SignUp" });
@@ -13,51 +11,26 @@ const signUp = async (req: Request, res: Response) => {
     }
     const { email, password, nickname } = req.body;
 
-    // * Validate if email already exists
-    let isEmailExist = false;
-    await User.findOne({ where: { email: email } })
-        .then((data) => {
-            if (data) {
-                isEmailExist = true;
-                res.status(400).send({
-                    status: 400,
-                    message: "Email already exist",
-                });
-            }
-        })
-        .catch((e) => {
-            console.log(e);
-            res.status(400).send({ status: 400, message: "Server Error" });
-            return;
+    const returnData: serviceReturnForm = await signUpService(
+        email,
+        password,
+        nickname
+    );
+    if (returnData.status == 200) {
+        // when successed
+        const { status, message, responseData } = returnData;
+        res.status(status).send({
+            status,
+            message,
+            responseData,
         });
-
-    // * Create User only when the email does not already exists
-    dotenv.config();
-    const TOKEN_KEY = process.env.TOKEN_KEY || "";
-    if (!isEmailExist) {
-        // * Encrypt user password
-        let encryptedPassword = await bcrypt.hash(password, 10);
-        const token = jwt.sign({ email }, TOKEN_KEY, {
-            expiresIn: "999999h",
+    } else {
+        // when failed
+        const { status, message } = returnData;
+        res.status(status).send({
+            status,
+            message,
         });
-        new User({
-            email: email || "",
-            password: encryptedPassword || "",
-            nickname: nickname || "",
-            android_token: token || "",
-        })
-            .save()
-            .then((data) => {
-                res.status(200).send({
-                    status: 200,
-                    message: "SignUp Success",
-                    token: data.android_token,
-                });
-            })
-            .catch((e) => {
-                console.log(e);
-                res.status(400).send({ status: 400, message: "Server Error" });
-            });
     }
 };
 
@@ -71,52 +44,23 @@ const login = async (req: Request, res: Response) => {
         return;
     }
     const { email, password } = req.body;
-
-    await User.findOne({
-        where: {
-            email: email,
-        },
-        attributes: ["id", "password", "android_token"],
-    })
-        .then(
-            async (data) => {
-                // * Validate if email already exists
-                if (data) {
-                    const isPasswordCorrect = await bcrypt.compare(
-                        password,
-                        data.password
-                    );
-                    // * Validate if password is correct
-                    if (isPasswordCorrect) {
-                        res.status(200).send({
-                            status: 200,
-                            message: "Login Success",
-                            token: data.android_token,
-                        });
-                    } else {
-                        res.status(400).send({
-                            status: 400,
-                            message: "Wrong password",
-                        });
-                    }
-                } else {
-                    res.status(400).send({
-                        status: 400,
-                        message: "Wrong email",
-                    });
-                }
-            },
-            (e) => {
-                throw e;
-            }
-        )
-        .catch((e) => {
-            console.log(e);
-            res.status(400).send({
-                status: 400,
-                message: "Server Error",
-            });
+    const returnData: serviceReturnForm = await loginService(email, password);
+    if (returnData.status == 200) {
+        // when successed
+        const { status, message, responseData } = returnData;
+        res.status(status).send({
+            status,
+            message,
+            responseData,
         });
+    } else {
+        // when failed
+        const { status, message } = returnData;
+        res.status(status).send({
+            status,
+            message,
+        });
+    }
 };
 
 export default {
