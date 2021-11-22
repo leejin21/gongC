34b471b7f19837802a34da65a69c63923ddec946
JIN <leejin21c@sookmyung.ac.kr>
2021-11-22 18:52:06 +0900

    [server]: auth 부분 비밀번호 bcrypt 적용, token 적용 완료

--- server/src/controllers/auth-controller.js
+++ server/src/controllers/auth-controller.js
@@ -1,51 +1,124 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
 const user_model_1 = __importDefault(require("../models/user.model"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const signUp = async (req, res) => {
     // TODO make, save, and send token
-    console.log("auth/signup");
-    console.log(req.body);
-    new user_model_1.default({
-        email: req.body.email || "",
-        password: req.body.password || "",
-        nickname: req.body.nickname || "",
-    })
-        .save()
-        .then(() => {
-        res.status(200).send({ status: 200, message: "회원가입 성공" });
+    // * Validate user input
+    if (!req.body.email || !req.body.password || !req.body.nickname) {
+        res.status(400).send({ status: 400, message: "Fail SignUp" });
+        return;
+    }
+    const { email, password, nickname } = req.body;
+    // * Validate if email already exists
+    let isEmailExist = false;
+    await user_model_1.default.findOne({ where: { email: email } })
+        .then((data) => {
+        if (data) {
+            isEmailExist = true;
+            res.status(400).send({
+                status: 400,
+                message: "Email already exist",
+            });
+        }
     })
         .catch((e) => {
         console.log(e);
-        res.status(400).send({ status: 400, message: "회원가입 실패" });
+        res.status(400).send({ status: 400, message: "Server Error" });
+        return;
     });
-};
-const login = async (req, res) => {
-    console.log(req.body, req.params);
-    // console.log(req);
-    await user_model_1.default.findOne({
-        where: {
-            email: req.body.email || "",
-            password: req.body.password || "",
-        },
-        attributes: ["id"],
-    })
-        .then((data) => {
-        if (data) {
-            console.log("login success");
+    // * Create User only when the email does not already exists
+    dotenv.config();
+    const TOKEN_KEY = process.env.TOKEN_KEY || "";
+    if (!isEmailExist) {
+        // * Encrypt user password
+        let encryptedPassword = await bcryptjs_1.default.hash(password, 10);
+        const token = jsonwebtoken_1.default.sign({ email }, TOKEN_KEY, {
+            expiresIn: "999999h",
+        });
+        new user_model_1.default({
+            email: email || "",
+            password: encryptedPassword || "",
+            nickname: nickname || "",
+            android_token: token || "",
+        })
+            .save()
+            .then((data) => {
             res.status(200).send({
                 status: 200,
-                message: "로그인 성공",
-                data: data.id,
+                message: "SignUp Success",
+                token: data.android_token,
             });
+        })
+            .catch((e) => {
+            console.log(e);
+            res.status(400).send({ status: 400, message: "Server Error" });
+        });
+    }
+};
+const login = async (req, res) => {
+    // * Validate user input
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({
+            status: 400,
+            message: "email and password is both required",
+        });
+        return;
+    }
+    const { email, password } = req.body;
+    await user_model_1.default.findOne({
+        where: {
+            email: email,
+        },
+        attributes: ["id", "password", "android_token"],
+    })
+        .then(async (data) => {
+        // * Validate if email already exists
+        if (data) {
+            const isPasswordCorrect = await bcryptjs_1.default.compare(password, data.password);
+            // * Validate if password is correct
+            if (isPasswordCorrect) {
+                res.status(200).send({
+                    status: 200,
+                    message: "Login Success",
+                    token: data.android_token,
+                });
+            }
+            else {
+                res.status(400).send({
+                    status: 400,
+                    message: "Wrong password",
+                });
+            }
         }
         else {
-            console.log("login fail: no email in db");
             res.status(400).send({
                 status: 400,
-                message: "로그인 실패",
+                message: "Wrong email",
             });
         }
     }, (e) => {
@@ -55,7 +128,7 @@ const login = async (req, res) => {
         console.log(e);
         res.status(400).send({
             status: 400,
-            message: "로그인 실패",
+            message: "Server Error",
         });
     });
 };
--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,50 +1,108 @@
 import { Response, Request } from "express";
-import { emit } from "process";
+import * as dotenv from "dotenv";
 import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const signUp = async (req: Request, res: Response) => {
     // TODO make, save, and send token
-    console.log("auth/signup");
-    console.log(req.body);
-    new User({
-        email: req.body.email || "",
-        password: req.body.password || "",
-        nickname: req.body.nickname || "",
-    })
-        .save()
-        .then(() => {
-            res.status(200).send({ status: 200, message: "회원가입 성공" });
+    // * Validate user input
+    if (!req.body.email || !req.body.password || !req.body.nickname) {
+        res.status(400).send({ status: 400, message: "Fail SignUp" });
+        return;
+    }
+    const { email, password, nickname } = req.body;
+
+    // * Validate if email already exists
+    let isEmailExist = false;
+    await User.findOne({ where: { email: email } })
+        .then((data) => {
+            if (data) {
+                isEmailExist = true;
+                res.status(400).send({
+                    status: 400,
+                    message: "Email already exist",
+                });
+            }
         })
         .catch((e) => {
             console.log(e);
-            res.status(400).send({ status: 400, message: "회원가입 실패" });
+            res.status(400).send({ status: 400, message: "Server Error" });
+            return;
         });
+
+    // * Create User only when the email does not already exists
+    dotenv.config();
+    const TOKEN_KEY = process.env.TOKEN_KEY || "";
+    if (!isEmailExist) {
+        // * Encrypt user password
+        let encryptedPassword = await bcrypt.hash(password, 10);
+        const token = jwt.sign({ email }, TOKEN_KEY, {
+            expiresIn: "999999h",
+        });
+        new User({
+            email: email || "",
+            password: encryptedPassword || "",
+            nickname: nickname || "",
+            android_token: token || "",
+        })
+            .save()
+            .then((data) => {
+                res.status(200).send({
+                    status: 200,
+                    message: "SignUp Success",
+                    token: data.android_token,
+                });
+            })
+            .catch((e) => {
+                console.log(e);
+                res.status(400).send({ status: 400, message: "Server Error" });
+            });
+    }
 };
 
 const login = async (req: Request, res: Response) => {
-    console.log(req.body, req.params);
-    // console.log(req);
+    // * Validate user input
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({
+            status: 400,
+            message: "email and password is both required",
+        });
+        return;
+    }
+    const { email, password } = req.body;
+
     await User.findOne({
         where: {
-            email: req.body.email || "",
-            password: req.body.password || "",
+            email: email,
         },
-        attributes: ["id"],
+        attributes: ["id", "password", "android_token"],
     })
         .then(
-            (data) => {
+            async (data) => {
+                // * Validate if email already exists
                 if (data) {
-                    console.log("login success");
-                    res.status(200).send({
-                        status: 200,
-                        message: "로그인 성공",
-                        data: data.id,
-                    });
+                    const isPasswordCorrect = await bcrypt.compare(
+                        password,
+                        data.password
+                    );
+                    // * Validate if password is correct
+                    if (isPasswordCorrect) {
+                        res.status(200).send({
+                            status: 200,
+                            message: "Login Success",
+                            token: data.android_token,
+                        });
+                    } else {
+                        res.status(400).send({
+                            status: 400,
+                            message: "Wrong password",
+                        });
+                    }
                 } else {
-                    console.log("login fail: no email in db");
                     res.status(400).send({
                         status: 400,
-                        message: "로그인 실패",
+                        message: "Wrong email",
                     });
                 }
             },
@@ -56,7 +114,7 @@ const login = async (req: Request, res: Response) => {
             console.log(e);
             res.status(400).send({
                 status: 400,
-                message: "로그인 실패",
+                message: "Server Error",
             });
         });
 };
