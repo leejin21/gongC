2bf10fa9d14b41a0180f3cb82b5e8399c05d727a
JIN <leejin21c@sookmyung.ac.kr>
2021-11-29 00:44:20 +0900

    [android]: (feat) 첫 화면에서 로그인, 회원가입으로 화면 전환

--- gongC/app/src/main/java/com/example/gongc/MainActivity.kt
+++ gongC/app/src/main/java/com/example/gongc/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.gongc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.gongc.feature.sign.SignInActivity
+import com.example.gongc.feature.sign.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnSignIn = findViewById<Button>(R.id.button_main_signin)
+        val btnSignUp = findViewById<Button>(R.id.button_main_signup)
+
+        btnSignIn.setOnClickListener{
+            Log.d("debug11", "main login")
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        btnSignUp.setOnClickListener{
+            Log.d("debug11", "main signup")
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
