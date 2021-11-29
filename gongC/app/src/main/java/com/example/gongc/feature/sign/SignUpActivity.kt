91f465f2e906fca2c5518d0d6b60bc5139a4bdfe
JIN <leejin21c@sookmyung.ac.kr>
2021-11-29 13:49:16 +0900

    [android]: (feat) 회원가입 화면에서 홈 화면으로 이동

--- gongC/app/src/main/java/com/example/gongc/feature/sign/SignUpActivity.kt
+++ gongC/app/src/main/java/com/example/gongc/feature/sign/SignUpActivity.kt
@@ -1,12 +1,20 @@
 package com.example.gongc.feature.sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.gongc.R
+import com.example.gongc.feature.home.HomeActivity
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val btnSignUp = findViewById<Button>(R.id.button_signup)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
